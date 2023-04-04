fdede977b03bd5049fa5ddc858b205639343ce00
shubham-hmh <shubham_203054@saitm.org>
2023-04-04 10:50:00 +0530

    first commit

---
+++ routes/authRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { createUser, loginUserCtrl, logout } = require("../controller/userCtrl");
+
+const router = express.Router();
+
+router.post("/register", createUser);
+
+
+
+router.post("/login", loginUserCtrl);
+router.get("/logout",logout);
+
+
+
+
+
+
+module.exports = router;
